c2c3611ae9889d39f056d8e7071159b60e6b0e2d
rushi1225 <rushibhingare25@gmail.com>
2024-12-14 14:09:56 +0530

    draw2

---
+++ script.js
@@ -0,0 +1,29 @@
+const boxes = document.querySelectorAll(".box"); // [,,,,,,.....]
+const colorInput = document.getElementById("color-input");
+
+let canDraw = false;
+let color = "black";
+
+document.addEventListener("mousedown", () => {
+  canDraw = true;
+});
+
+document.addEventListener("mouseup", () => {
+  canDraw = false;
+});
+
+for (let i = 0; i < 2500; i++) {
+  boxes[i].addEventListener("mouseover", () => {
+    if (canDraw) {
+      boxes[i].style.backgroundColor = color;
+    }
+  });
+}
+
+colorInput.addEventListener("change", () => {
+  color = colorInput.value;
+});
+
+function erase() {
+  color = "white";
+}
